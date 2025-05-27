8e43abcb1d50e2cf86c21b66c91c9753d432d710
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-05-27 14:20:31 +0200

    Add main.py file to launch experiments

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import shutil
+
+import pyrootutils
+
+from omegaconf import DictConfig
+import hydra
+from hydra.utils import call
+import wandb
+
+import utils
+
+
+@hydra.main(version_base = None, config_path = "../config", config_name = "config")
+def main(config: DictConfig):
+    utils.preprocess_config(config)
+    utils.setup_wandb(config)
+    call(config.exp.run_func, config)
+    wandb.finish()
+    if config.exp.cleanup:
+        shutil.rmtree(config.exp.log_dir, ignore_errors=True)
+
+if __name__ == "__main__":
+    pyrootutils.setup_root(
+        search_from=__file__,
+        indicator=".project-root",
+        project_root_env_var=True,
+        dotenv=True,
+        pythonpath=True,
+        cwd=True,
+    )
+    main()
