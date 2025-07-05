5a00e1b3388e14a49482cf0b80178e7733841b46
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-07-05 15:24:54 +0200

    Add grid search scripts for TinyImageNet

---
+++ scripts/tiny_imagenet/train/train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=shield_train_tiny_imagenet
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/tiny_imagenet/train"
