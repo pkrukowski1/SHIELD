24c6b62af57af23f7c7184b2b490098934ae087c
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-07-05 14:25:23 +0200

    Add grid search scripts for Rotated MNIST

---
+++ scripts/rotated_mnist/train/train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=shield_train_rotated_mnist
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=dgx
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/rotated_mnist/train"
