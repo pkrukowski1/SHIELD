72d607e1b53da639e43512d3ec965365c2656a96
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-07-07 16:36:18 +0200

    Fix paths

--- src/utils/hydra.py
+++ src/utils/hydra.py
@@ -6,16 +6,20 @@ from omegaconf import DictConfig
 
 def extract_output_dir(config: DictConfig) -> Path:
     """
-    Extracts path to output directory created by Hydra as pathlib.Path instance
+    Extracts path to output directory as pathlib.Path and ensures it exists.
     """
-    
     date = '/'.join(list(config._metadata.resolver_cache['now'].values()))
-    output_dir = Path.cwd() / os.environ['OUTPUT_DIR'] / date
+
+    output_dir = Path(os.environ['OUTPUT_DIR']) / date
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+
     return output_dir
+
+
 
 def preprocess_config(config: DictConfig):
     """
-    Sets config.exp.log_dir to date extracted from metadata.
+    Sets config.exp.log_dir to date-extracted output path.
     """
-
     config.exp.log_dir = extract_output_dir(config)
