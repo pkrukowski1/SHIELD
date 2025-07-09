c8489b093a8dd7c682ff1b2c241916600a79cea1
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-07-09 21:27:47 +0200

    Fix problem with reproducibility

--- src/utils/fabric.py
+++ src/utils/fabric.py
@@ -3,6 +3,8 @@ from omegaconf import DictConfig
 
 from lightning.fabric import Fabric
 
+import torch
+
 
 def setup_fabric(config: DictConfig) -> Fabric:
     """
@@ -11,5 +13,7 @@ def setup_fabric(config: DictConfig) -> Fabric:
 
     fabric = instantiate(config.fabric)
     fabric.seed_everything(config.exp.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     fabric.launch()
     return fabric
