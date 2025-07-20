9796ceb907e83824822b86f6b06d32e15dc68dd0
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-07-20 13:09:48 +0200

    Implement experiment with FGSM

--- src/experiment/__init__.py
+++ src/experiment/__init__.py
@@ -1,3 +1,4 @@
 from .interval_training import experiment as interval_training
 from .calc_ver_accuracy import experiment as calc_ver_accuracy
 from .adversarial_attack import experiment as adversarial_attack
+from .inc_pert_size_fgsm import experiment as inc_pert_size_fgsm
