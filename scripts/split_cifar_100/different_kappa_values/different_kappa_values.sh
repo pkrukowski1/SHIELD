09679d9d2ac8e4c00ad1dc0fafb1825cd2385a4a
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-10-10 18:33:35 +0200

    Add scripts for additional ablation study

---
+++ scripts/split_cifar_100/different_kappa_values/different_kappa_values.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=shield_interval_mixup_split_cifar_100_different_kappa_values
+#SBATCH --qos=normal
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=rtx4090
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/split_cifar_100/different_kappa_values/different_kappa_values"
